876ee98f5b6f2b6766dcc600ddfffa63833a5ffd
Romeo <45272844+Riki1200@users.noreply.github.com>
2023-04-21 01:51:46 -0400

    init repot

--- lib/session_manager.dart
+++ lib/session_manager.dart
@@ -1,28 +1,110 @@
 library session_manager;
 
+import 'dart:html';
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  final void Function()? onPressed;
+class SessionActivityManager extends StatefulWidget {
   final Widget child;
-  ButtonStyle? style;
-  CustomButton(
-      {Key? key, @required this.onPressed, required this.child, this.style})
-      : super(key: key) {
-    assert(onPressed != null);
-    style = TextButton.styleFrom(
-      foregroundColor: Colors.white,
-      padding: const EdgeInsets.all(16.0),
-      backgroundColor: Colors.blue,
-      elevation: 9.0,
-      textStyle: const TextStyle(
-        fontSize: 20,
-      ),
-    );
+
+  final void Function()? onSessionExpired;
+  final void Function(Timer?)? onSessionTimeout;
+  final void Function(AppLifecycleState state)? onSessionActivity;
+  final Duration sessionTimeout;
+  final HitTestBehavior? behavior;
+
+  const SessionActivityManager({
+    Key? key,
+    required this.child,
+    this.onSessionExpired,
+    this.onSessionTimeout,
+    this.onSessionActivity,
+    this.sessionTimeout = const Duration(minutes: 5),
+    this.behavior = HitTestBehavior.deferToChild,
+  }) : super(key: key);
+
+  @override
+  SessionActivityManagerState createState() => SessionActivityManagerState();
+}
+
+class SessionActivityManagerState extends State<SessionActivityManager>
+    with WidgetsBindingObserver {
+  Timer? _timer = null;
+  final FocusNode focusNode = FocusNode();
+
+  HitTestBehavior defualtBehavior = HitTestBehavior.translucent;
+
+  @override
+  void initState() {
+    if (kIsWeb) {
+      window.addEventListener('focus', onFocus);
+      window.addEventListener('blur', onBlur);
+    } else {
+      WidgetsBinding.instance.addObserver(this);
+    }
+    super.initState();
+    _initializeTimer();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return TextButton(onPressed: onPressed, style: style, child: child);
+  void didChangeMetrics() {
+    setState(() {
+      [];
+    });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) =>
+      widget.onSessionActivity?.call(state);
+
+  @override
+  void dispose() {
+    if (kIsWeb) {
+      window.removeEventListener('focus', onFocus);
+      window.removeEventListener('blur', onBlur);
+    } else {
+      WidgetsBinding.instance.removeObserver(this);
+    }
+    super.dispose();
+  }
+
+  void onFocus(Event e) {
+    didChangeAppLifecycleState(AppLifecycleState.resumed);
+  }
+
+  void onBlur(Event e) {
+    didChangeAppLifecycleState(AppLifecycleState.paused);
+  }
+
+  @override
+  Widget build(BuildContext context) => Listener(
+      key: widget.key,
+      onPointerMove: (event) {
+        _handleUserInteraction();
+      },
+      behavior: widget.behavior ?? defualtBehavior,
+      child: GestureDetector(
+        key: const Key('session_manager'),
+        onTap: () => _handleUserInteraction(),
+        onPanDown: (_) => _handleUserInteraction(),
+        onScaleStart: (_) => _handleUserInteraction(),
+        // ... repeat this for all gesture events
+        behavior: HitTestBehavior.translucent,
+        child: widget.child,
+      ));
+
+  void _handleUserInteraction() {
+    _initializeTimer();
+  }
+
+  void _initializeTimer() {
+    _timer?.cancel();
+    _timer = Timer.periodic(widget.sessionTimeout, (timer) {
+      widget.onSessionExpired?.call();
+    });
+
+    widget.onSessionTimeout?.call(_timer);
   }
 }
--- test/session_manager_test.dart
+++ test/session_manager_test.dart
@@ -1,12 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter_test/flutter_test.dart';
 import 'package:session_manager/session_manager.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('render widget', () {
+    testWidgets('renders widget', (WidgetTester tester) async {});
   });
 }
